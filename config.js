68ef1a1d1aa5b07bce772286ce38e919ff85dfd4
LeeEunnSu0011 <luke190256@gmail.com>
2026-02-22 07:17:50 +0000

    config.js 추가 (도메인 제한으로 키 보호)

---
+++ config.js
@@ -0,0 +1,10 @@
+// config.js - 설정 파일
+export const CONFIG = {
+  dataUrl: './data.json',
+  cacheKey: 'epg_cache_v10',
+  posterCacheKey: 'poster_cache_v2',
+  cacheAgeMin: 30,
+  updateSchedule: [0, 6, 12, 18],
+  tmdbKey: '0a3e13faef62e13445d8744883186dbb',
+  tmdbImg: 'https://image.tmdb.org/t/p/w200',
+};
