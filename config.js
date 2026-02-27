681d8033988105420b318f15a192cd6ecc905604
LeeEunSu0011 <luke190256@gmail.com>
2026-02-27 10:15:11 +0000

    fix: detailCacheKey 버전 올려 캐시 초기화

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ export const CONFIG = {
   dataUrl: './data.json',
   cacheKey: 'epg_cache_v11',
   posterCacheKey: 'poster_cache_v3',
-  detailCacheKey: 'detail_cache_v1',
+  detailCacheKey: 'detail_cache_v2',
   cacheAgeMin: 30,
   updateSchedule: [0, 6, 12, 18],
   tmdbKey: '0a3e13faef62e13445d8744883186dbb',
